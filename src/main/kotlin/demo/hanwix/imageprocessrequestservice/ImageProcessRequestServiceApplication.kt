4caea4afef287c8255ff8b5e5774976a82e8160e
hanwix2 <alexsjh23@gmail.com>
2026-03-08 17:17:21 +0900

    Init Project

---
+++ src/main/kotlin/demo/hanwix/imageprocessrequestservice/ImageProcessRequestServiceApplication.kt
@@ -0,0 +1,11 @@
+package demo.hanwix.imageprocessrequestservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImageProcessRequestServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ImageProcessRequestServiceApplication>(*args)
+}
---
+++ src/test/kotlin/demo/hanwix/imageprocessrequestservice/ImageProcessRequestServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package demo.hanwix.imageprocessrequestservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImageProcessRequestServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
