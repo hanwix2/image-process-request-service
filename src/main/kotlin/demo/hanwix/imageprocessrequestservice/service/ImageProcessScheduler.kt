df1f052d295e21e78ecb01eaf625d9f3e81896d1
hanwix2 <alexsjh23@gmail.com>
2026-03-09 18:40:17 +0900

    Change time term of synchronizing task status scheduler
    - 30min -> 5min

--- src/main/kotlin/demo/hanwix/imageprocessrequestservice/service/ImageProcessScheduler.kt
+++ src/main/kotlin/demo/hanwix/imageprocessrequestservice/service/ImageProcessScheduler.kt
@@ -9,12 +9,14 @@ class ImageProcessScheduler(
     private val outboxAppender: OutboxAppender
 ) {
 
+    // Transactional Outbox 패턴 적용 & 메시지 발행을 위한 스케줄러 (1분 주기)
     @Scheduled(fixedDelay = 1000)
     fun publishPendingOutboxMessages() {
         outboxAppender.publishPendingMessages()
     }
 
-    @Scheduled(fixedDelay = 30000)
+    // 이미지 처리 진행중인 작업의 상태를 업데이트하는 스케줄러 (5분 주기)
+    @Scheduled(fixedDelay = 5000)
     fun syncProcessingTaskStatuses() {
         imageProcessService.syncProcessingTaskStatuses()
     }
